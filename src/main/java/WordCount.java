a35d7ee4b1a0a3e176b2feff36bfb48ba2fda5ff
daugraph <daugraph@qq.com>
2021-04-14 21:12:09 +0800

    modified:   src/main/java/WordCount.java

--- src/main/java/WordCount.java
+++ src/main/java/WordCount.java
@@ -21,6 +21,7 @@ public class WordCount {
 
         public void map(Object key, Text value, Context context
         ) throws IOException, InterruptedException {
+            System.out.println("daugraph");
             StringTokenizer itr = new StringTokenizer(value.toString());
             while (itr.hasMoreTokens()) {
                 word.set(itr.nextToken());
